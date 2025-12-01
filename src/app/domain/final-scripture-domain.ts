b7f53e6104fde26ac0d4187d588d4de899624a64
António Conselheiro <anarcanudos@proton.me>
2025-12-01 06:27:57 -0300

    mudando a base em hebraico HSB incorreta para uma versão HSB4

---
+++ src/app/domain/final-scripture-domain.ts
@@ -0,0 +1,479 @@
+export type OldTestamentBookList = 
+  'Genesis' |
+  'Exodo' |
+  'Levitico' |
+  'Numeros' |
+  'Deuteronomio' |
+  'Josue' |
+  'Juizes' |
+  'Rute' |
+  '1Samuel' |
+  '2Samuel' |
+  '1Reis' |
+  '2Reis' |
+  '1Cronicas' |
+  '2Cronicas' |
+  'Esdras' |
+  'Neemias' |
+  'Ester' |
+  'Jo' |
+  'Salmos' |
+  'Proverbios' |
+  'Eclesiastes' |
+  'Canticos' |
+  'Isaias' |
+  'Jeremias' |
+  'Lamentacoes' |
+  'Ezequiel' |
+  'Daniel' |
+  'Oseias' |
+  'Joel' |
+  'Amos' |
+  'Obadias' |
+  'Jonas' |
+  'Miqueias' |
+  'Naum' |
+  'Habacuque' |
+  'Sofonias' |
+  'Ageu' |
+  'Zacarias' |
+  'Malaquias';
+
+export type NewTestamentBookList = 'Mateus' |
+  'Marcos' |
+  'Lucas' |
+  'Joao' |
+  'Atos' |
+  'Romanos' |
+  '1Corintios' |
+  '2Corintios' |
+  'Galatas' |
+  'Efesios' |
+  'Filipenses' |
+  'Colossenses' |
+  '1Tessalonicenses' |
+  '2Tessalonicenses' |
+  '1Timoteo' |
+  '2Timoteo' |
+  'Tito' |
+  'Filemom' |
+  'Hebreus' |
+  'Tiago' |
+  '1Pedro' |
+  '2Pedro' |
+  '1Joao' |
+  '2Joao' |
+  '3Joao' |
+  'Judas' |
+  'Apocalipse';
+
+export type MahabharataBookNames =
+  'Adi_Parva' |
+  'Sabha_Parva' |
+  'Vana_Parva' |
+  'Virata_Parva' |
+  'Udyoga_Parva' |
+  'Bhishma_Parva' |
+  'Drona_Parva' |
+  'Karna_Parva' |
+  'Shalya_Parva' |
+  'Sauptika_Parva' |
+  'Stri_Parva' |
+  'Shanti_Parva' |
+  'Anushasana_Parva' |
+  'Ashvamedhika_Parva' |
+  'Ashramavasika_Parva' |
+  'Mausala_Parva' |
+  'Mahaprasthanika_Parva' |
+  'Svargarohana_Parva' |
+  'Harivamsa_Parva';
+
+export type DeadSeaManuscriptsOldTestament =  '1QIsa_a_Grande_Rolo_de_Isaias' |
+'1QIsa_b_Rolo_de_Isaias_B' |
+'4QGen_Exod_Fragmentos_de_Genefis_e_Exodo' |
+'4QPs_a_Salmos_Apocrifos' |
+'11QPs_a_Grande_Rolo_de_Salmos' |
+'4QJer_b_Jeremias_Aramaico' |
+'4QDan_b_Daniel_Fragmentos' |
+'4QSam_a_Samuel_A' |
+'4QSam_b_Samuel_B';
+
+export type DeadSeaManuscriptsFogottenProphecies = '1QapGen_Genesis_Apocrifo'
+ | '4Q201_Enoque_Aramaico_1'
+ | '4Q202_Enoque_Aramaico_2'
+ | '4Q203_Enoque_Aramaico_3'
+ | '4Q204_Enoque_Aramaico_4'
+ | '4Q205_Enoque_Aramaico_5'
+ | '4Q206_Enoque_Aramaico_6'
+ | '4Q207_Enoque_Aramaico_7'
+ | '4Q216_Livro_dos_Jubileus_1'
+ | '4Q217_Livro_dos_Jubileus_2'
+ | '4Q218_Livro_dos_Jubileus_3'
+ | '4Q219_Livro_dos_Jubileus_4'
+ | '4Q220_Livro_dos_Jubileus_5'
+ | '4Q221_Livro_dos_Jubileus_6'
+ | '4Q222_Livro_dos_Jubileus_7'
+ | '4Q223_Livro_dos_Jubileus_8'
+ | '4Q224_Livro_dos_Jubileus_9'
+ | '4Q242_Oracao_de_Nabonido'
+ | '4Q196_Tobias_Aramaico_1'
+ | '4Q197_Tobias_Aramaico_2'
+ | '4Q198_Tobias_Aramaico_3'
+ | '4Q199_Tobias_Aramaico_4'
+ | '4Q200_Tobias_Hebraico';
+
+export type DeadSeaManuscriptsCommunity = '1QS_Regra_da_Comunidade' | 
+'1QSa_Regra_da_Congregacao' | 
+'1QSb_Regra_das_Bencaos' | 
+'1QH_Hinos_de_Accao_de_Gracas' | 
+'CD_Documento_de_Damasco' | 
+'4Q266_Damasco_1' | 
+'4Q267_Damasco_2' | 
+'4Q268_Damasco_3' | 
+'4Q269_Damasco_4' | 
+'4Q270_Damasco_5' | 
+'4Q271_Damasco_6' | 
+'4Q272_Damasco_7' | 
+'4Q273_Damasco_8' | 
+'1QM_Rolo_da_Guerra' | 
+'11QTemple_Rolo_do_Templo' | 
+'4QMMT_Miqsat_Maase_ha_Torah' |
+'4QMMT_Miqsat_Maase_ha_Torah' |
+'4Q159_Parafrase_de_Deuteronomio' |
+'4Q251_Texto_Halakhico_1' |
+'4Q252_Texto_Halakhico_2' |
+'1QpHab_Pesher_Habacuque' |
+'4QpIsa_Pesher_Isaias' |
+'4QpHos_Pesher_Oseias' |
+'4QpNah_Pesher_Naum' |
+'4QpPs_Pesher_Salmos' |
+'4QpMal_Pesher_Malaquias' |
+'4QpZeph_Pesher_Sofonias' |
+'4QpMic_Pesher_Miqueias' |
+'3Q15_Rolo_de_Cobre' |
+'4Q448_Elogio_do_Rei_Jonata' |
+'4Q550_Texto_Historico_1' |
+'4Q551_Texto_Historico_2';
+
+export type DeadSeaManuscriptsSapiential = '4QInstruction_Instrucoes_Sapienciais' |
+'4Q417_Sapiencial_1' |
+'4Q418_Sapiencial_2' |
+'4Q419_Sapiencial_3' |
+'4Q400_Canticos_do_Sacrificio_do_Sabado_1' | 
+'4Q401_Canticos_do_Sacrificio_do_Sabado_2' | 
+'4Q402_Canticos_do_Sacrificio_do_Sabado_3' | 
+'4Q403_Canticos_do_Sacrificio_do_Sabado_4' | 
+'4Q404_Canticos_do_Sacrificio_do_Sabado_5' | 
+'4Q405_Canticos_do_Sacrificio_do_Sabado_6' | 
+'4Q406_Canticos_do_Sacrificio_do_Sabado_7' | 
+'4Q407_Canticos_do_Sacrificio_do_Sabado_8' | 
+'11QPs_a_Salmos_Apocrifos' | 
+'4QBerakhot_Texto_de_Bencaos';
+
+export type DeadSeaManuscriptsAstrological = '4QCalendario_Textos_Calendarios' | 
+'4QOtot_Sinais_Calendarios' | 
+'4QEnoch_Astronomico' | 
+'4QPesach_Texto_sobre_a_Pascoa' | 
+'4Q503_Oracoes_Calendaricas';
+
+export type DeadSeaManuscriptsDictionaries = '4QVocabulary_Listas_Lexicais' |
+'4Q185_Texto_Didatico' |
+'4Q186_Texto_Didatico_2';
+
+export type DeadSeaManuscriptsUntituled = '1Q1' | '1Q2' | '1Q3' | '1Q4' | '1Q5' | '1Q6' | '1Q7' | '1Q8' | '1Q9' | '1Q10' | '1Q11' | '1Q12' | '1Q13' | '1Q14' | '1Q15' | '1Q16' | '1Q17' | '1Q18' | '1Q19' | '1Q20' | '1Q21' | '1Q22' | '1Q23' | '1Q24' | '1Q25' | '1Q26' | '1Q27' | '1Q28' | '1Q29' | '1Q30' | '2Q1' | '2Q2' | '2Q3' | '2Q4' | '2Q5' | '2Q6' | '2Q7' | '2Q8' | '2Q9' | '2Q10' | '3Q1' | '3Q2' | '3Q3' | '3Q4' | '3Q5' | '3Q6' | '3Q7' | '3Q8' | '3Q9' | '3Q10' | '3Q11' | '3Q12' | '3Q13' | '3Q14' | '3Q15' | '4Q1' | '4Q2' | '4Q3' | '4Q4' | '4Q5' | '4Q6' | '4Q7' | '4Q8' | '4Q9' | '4Q10' | '4Q12' | '4Q13' | '4Q14' | '4Q15' | '4Q16' | '4Q17' | '4Q18' | '4Q19' | '4Q20' | '4Q21' | '4Q22' | '4Q23' | '4Q24' | '4Q25' | '4Q26' | '4Q27' | '4Q28' | '4Q29' | '4Q30' | '4Q31' | '4Q32' | '4Q33' | '4Q34' | '4Q35' | '4Q36' | '4Q37' | '4Q38' | '4Q39' | '4Q40' | '4Q41' | '4Q42' | '4Q43' | '4Q44' | '4Q45' | '4Q46' | '4Q47' | '4Q48' | '4Q49' | '4Q50' | '4Q51' | '4Q52' | '4Q53' | '4Q54' | '4Q55' | '4Q56' | '4Q57' | '4Q58' | '4Q59' | '4Q60' | '4Q61' | '4Q62' | '4Q63' | '4Q64' | '4Q65' | '4Q66' | '4Q67' | '4Q68' | '4Q69' | '4Q70' | '4Q71' | '4Q72' | '4Q73' | '4Q74' | '4Q75' | '4Q76' | '4Q77' | '4Q78' | '4Q79' | '4Q80' | '4Q81' | '4Q82' | '4Q84' | '4Q85' | '4Q86' | '4Q87' | '4Q88' | '4Q89' | '4Q90' | '4Q91' | '4Q92' | '4Q93' | '4Q94' | '4Q95' | '4Q96' | '4Q97' | '4Q98' | '4Q99' | '4Q100' | '4Q101' | '4Q102' | '4Q103' | '4Q104' | '4Q105' | '4Q106' | '4Q107' | '4Q108' | '4Q109' | '4Q110' | '4Q111' | '4Q112' | '4Q113' | '4Q114' | '4Q115' | '4Q116' | '4Q117' | '4Q118' | '4Q119' | '4Q120' | '4Q121' | '4Q122' | '4Q123' | '4Q124' | '4Q125' | '4Q126' | '4Q127' | '4Q128' | '4Q129' | '4Q130' | '4Q131' | '4Q132' | '4Q133' | '4Q134' | '4Q135' | '4Q136' | '4Q137' | '4Q138' | '4Q139' | '4Q140' | '4Q141' | '4Q142' | '4Q143' | '4Q144' | '4Q145' | '4Q146' | '4Q147' | '4Q148' | '4Q149' | '4Q150' | '4Q151' | '4Q152' | '4Q153' | '4Q154' | '4Q155' | '4Q156' | '4Q157' | '4Q160' | '4Q161' | '4Q162' | '4Q163' | '4Q164' | '4Q165' | '4Q166' | '4Q167' | '4Q168' | '4Q169' | '4Q170' | '4Q171' | '4Q172' | '4Q173' | '4Q174' | '4Q175' | '4Q176' | '4Q177' | '4Q178' | '4Q179' | '4Q180' | '4Q181' | '4Q182' | '4Q183' | '4Q184' | '4Q187' | '4Q188' | '4Q189' | '4Q190' | '4Q191' | '4Q192' | '4Q193' | '4Q194' | '4Q195' | '4Q201' | '4Q202' | '4Q203' | '4Q204' | '4Q205' | '4Q206' | '4Q207' | '4Q208' | '4Q209' | '4Q210' | '4Q211' | '4Q212' | '4Q213' | '4Q214' | '4Q215' | '4Q216' | '4Q217' | '4Q218' | '4Q219' | '4Q220' | '4Q221' | '4Q222' | '4Q223' | '4Q224' | '4Q225' | '4Q226' | '4Q227' | '4Q228' | '4Q229' | '4Q230' | '4Q231' | '4Q232' | '4Q233' | '4Q234' | '4Q235' | '4Q236' | '4Q237' | '4Q238' | '4Q239' | '4Q240' | '4Q241' | '4Q243' | '4Q244' | '4Q245' | '4Q246' | '4Q247' | '4Q248' | '4Q249' | '4Q250' | '4Q253' | '4Q254' | '4Q255' | '4Q256' | '4Q257' | '4Q258' | '4Q259' | '4Q260' | '4Q261' | '4Q262' | '4Q263' | '4Q264' | '4Q265' | '4Q274' | '4Q275' | '4Q276' | '4Q277' | '4Q278' | '4Q279' | '4Q280' | '4Q281' | '4Q282' | '4Q283' | '4Q284' | '4Q285' | '4Q286' | '4Q287' | '4Q288' | '4Q289' | '4Q290' | '4Q291' | '4Q292' | '4Q293' | '4Q294' | '4Q295' | '4Q296' | '4Q297' | '4Q298' | '4Q299' | '4Q300' | '4Q301' | '4Q302' | '4Q303' | '4Q304' | '4Q305' | '4Q306' | '4Q307' | '4Q308' | '4Q309' | '4Q310' | '4Q311' | '4Q312' | '4Q313' | '4Q314' | '4Q315' | '4Q316' | '4Q317' | '4Q318' | '4Q319' | '4Q320' | '4Q321' | '4Q322' | '4Q323' | '4Q324' | '4Q325' | '4Q326' | '4Q327' | '4Q328' | '4Q329' | '4Q330' | '4Q331' | '4Q332' | '4Q333' | '4Q334' | '4Q335' | '4Q336' | '4Q337' | '4Q338' | '4Q339' | '4Q340' | '4Q341' | '4Q342' | '4Q343' | '4Q344' | '4Q345' | '4Q346' | '4Q347' | '4Q348' | '4Q349' | '4Q350' | '4Q351' | '4Q352' | '4Q353' | '4Q354' | '4Q355' | '4Q356' | '4Q357' | '4Q358' | '4Q359' | '4Q360' | '4Q361' | '4Q362' | '4Q363' | '4Q364' | '4Q365' | '4Q366' | '4Q367' | '4Q368' | '4Q369' | '4Q370' | '4Q371' | '4Q372' | '4Q373' | '4Q374' | '4Q375' | '4Q376' | '4Q377' | '4Q378' | '4Q379' | '4Q380' | '4Q381' | '4Q382' | '4Q383' | '4Q384' | '4Q385' | '4Q386' | '4Q387' | '4Q388' | '4Q389' | '4Q390' | '4Q391' | '4Q392' | '4Q393' | '4Q394' | '4Q395' | '4Q396' | '4Q397' | '4Q398' | '4Q399' | '4Q400' | '4Q401' | '4Q402' | '4Q403' | '4Q404' | '4Q405' | '4Q406' | '4Q407' | '4Q408' | '4Q409' | '4Q410' | '4Q411' | '4Q412' | '4Q413' | '4Q414' | '4Q415' | '4Q416' | '4Q417' | '4Q418' | '4Q419' | '4Q420' | '4Q421' | '4Q422' | '4Q423' | '4Q424' | '4Q425' | '4Q426' | '4Q427' | '4Q428' | '4Q429' | '4Q430' | '4Q431' | '4Q432' | '4Q433' | '4Q434' | '4Q435' | '4Q436' | '4Q437' | '4Q438' | '4Q439' | '4Q440' | '4Q441' | '4Q442' | '4Q443' | '4Q444' | '4Q445' | '4Q446' | '4Q447' | '4Q448' | '4Q449' | '4Q450' | '4Q451' | '4Q452' | '4Q453' | '4Q454' | '4Q455' | '4Q456' | '4Q457' | '4Q458' | '4Q459' | '4Q460' | '4Q461' | '4Q462' | '4Q463' | '4Q464' | '4Q465' | '4Q466' | '4Q467' | '4Q468' | '4Q469' | '4Q470' | '4Q471' | '4Q472' | '4Q473' | '4Q474' | '4Q475' | '4Q476' | '4Q477' | '4Q478' | '4Q479' | '4Q480' | '4Q481' | '4Q482' | '4Q483' | '4Q484' | '4Q485' | '4Q486' | '4Q487' | '4Q488' | '4Q489' | '4Q490' | '4Q491' | '4Q492' | '4Q493' | '4Q494' | '4Q495' | '4Q496' | '4Q497' | '4Q498' | '4Q499' | '4Q500';
+
+export type EthiopicBibleBook = 'Genesis'
+  | 'Exodo'
+  | 'Leviticos'
+  | 'Numeros'
+  | 'Deuteronomio'
+  | 'Josue'
+  | 'Juizes'
+  | 'Rute'
+  | 'I_Samuel'
+  | 'II_Samuel'
+  | 'I_Reis'
+  | 'II_Reis'
+  | 'I_Cronicas'
+  | 'II_Cronicas'
+  | 'Jubileu'
+  | 'Enoque'
+  | 'Esdras_e_Nemias'
+  | 'Esdras_2'
+  | 'Esdras_Sutuel'
+  | 'Tobias'
+  | 'Judite'
+  | 'Ester'
+  | 'I_Macaqueus'
+  | 'II_Macaqueus'
+  | 'III_Macaqueus'
+  | 'Job'
+  | 'Salmos'
+  | 'Provérbios'
+  | 'Tegsats'
+  | 'Metsihafe_Tibeb'
+  | 'Eclesiastes'
+  | 'Cantares'
+  | 'Isaías'
+  | 'Jeremias'
+  | 'Ezequiel'
+  | 'Daniel'
+  | 'Oseias'
+  | 'Amos'
+  | 'Miquéias'
+  | 'Joel'
+  | 'Obadias'
+  | 'Jonas'
+  | 'Naum'
+  | 'Habacuque'
+  | 'Sofonias'
+  | 'Ageu'
+  | 'Zacarias'
+  | 'Malaquias'
+  | 'Josue_Filho_de_Sirac'
+  | 'Josefas_Filho_de_Bengorion'
+  | 'Mateus'
+  | 'Marcos'
+  | 'Lucas'
+  | 'Joao'
+  | 'Atos'
+  | 'Romanos'
+  | 'I_Corintios'
+  | 'II_Corintios'
+  | 'Gálatas'
+  | 'Efesios'
+  | 'Filipenses'
+  | 'Colossenses'
+  | 'I_Tessalonicenses'
+  | 'II_Tessalonicenses'
+  | 'I_Timoteo'
+  | 'II_Timoteo'
+  | 'Tito'
+  | 'Filemon'
+  | 'Hebreus'
+  | 'I_Pedro'
+  | 'II_Pedro'
+  | 'I_Joao'
+  | 'II_Joao'
+  | 'III_Joao'
+  | 'Tiago'
+  | 'Judas'
+  | 'Apocalipse'
+  | 'Sirate_Tsion'
+  | 'Tizaz'
+  | 'Gitsew'
+  | 'Abtilis'
+  | 'I_Book_of_Dominos'
+  | 'II_Book_of_Dominos'
+  | 'Book_of_Clement'
+  | 'Didascalia';
+
+  export type EthiopicLiturgicalBooks =
+  | 'Livro_Liturgico_com_14_Anaphora'
+  | 'Ghitsacwa_Leccionarios'
+  | 'Livro_do_Sacramento_do_Casamento_Metsehafe_Teklil'
+  | 'Livro_do_Batismo'
+  | 'Livro_da_Ordenacao'
+  | 'Livro_do_Pacto'
+  | 'Livro_de_Oracoes_para_os_Mortos'
+  | 'Livro_de_Oracoes_do_Incenso'
+  | 'Livro_do_Canon'
+  | 'Livro_de_Oracoes_de_Purificacao'
+  | 'Metsehafe_Menequsat'
+  | 'Digua'
+  | 'Thesome_Digua'
+  | 'Mieraf'
+  | 'Zimare'
+  | 'Mewasiet'
+  | 'Zik'
+  | 'Mezmur'
+  | 'Liturgy_Kedasie'
+  | 'Saatat_de_ABBA_Giorgis'
+  | 'Livro_de_Abushakir'
+  | 'Sid_Filho_de_Batrik'
+  | 'Mark_Filho_de_Kenbar'
+  | 'Leader_of_Blind_por_Demetros'
+  | 'Matematica_sobre_calendario_Monasterio_Bizen';
+
+export type EthiopicTheologicalBooks =
+  | 'Haymanote_Abew_Faith_of_the_Fathers'
+  | 'Works_of_St_Cyril'
+  | 'Exegese_da_Carta_aos_Hebreus_St_John_Chrysostom'
+  | 'Pastoral_Work_St_John_Chrysostom'
+  | 'Severious_of_Asmunage'
+  | 'Hilawae_Melekote'
+  | 'Livro_de_Hawi'
+  | 'Livro_do_Misterio_Abba_Georgis'
+  | 'Religious_Documentary_Book_Jacob_of_Elbaredia'
+  | 'A_True_Faith_Zera_Yacob'
+  | 'Five_Pillars_of_Sacraments_Catechism'
+  | 'Didache_and_Abthulis'
+  | 'Livro_do_Sinodo_dos_Apostolos'
+  | 'Livro_do_Sinodo_de_Nicea'
+  | 'Livro_do_Sinodo_de_Galatia'
+  | 'Livro_do_Sinodo_de_Antioquia'
+  | 'Livro_do_Sinodo_de_Lethokia'
+  | 'Livro_do_Sinodo_de_Kerthica'
+  | 'Livro_do_Sinodo_de_Esrskousia'
+  | 'Livro_do_Sinodo_de_Srethia'
+  | 'Fetha_Negast'
+  | 'Spiritual_Medicine_Fewse_Menfsawi'
+  | 'Exegese_sobre_reunioes_clericais_Tikbe_Kahenat';
+
+export type NagHammadiTexts =
+  // Códice I
+  | 'Evangelho_de_Tomas'
+  | 'Evangelho_de_Filipenses'
+  | 'Hipóstase_dos_Arquontes'
+  | 'Da_Origem_do_Mundo'
+  | 'Exegese_do_Salmo'
+  | 'Segunda_Parte_do_Evangelho_de_Joao'
+  | 'Interpretacao_da_Sinfonia'
+
+  // Códice II
+  | 'Livro_Secreto_de_Joao'
+  | 'Apócrifo_de_Joao'
+  | 'Evangelho_dos_Hebreus'
+  | 'Evangelho_dos_Egípcios'
+
+  // Códice III
+  | 'Protênnoia_Triforme'
+  | 'Evangelho_da_Verdade'
+  | 'Sofia_de_Jesus_Cristo'
+  | 'Epístola_de_Joao'
+  | 'Paráfrase_do_Pastor'
+
+  // Códice IV
+  | 'Trovão_Paz_e_o_Homem_Perfeito'
+  | 'Evangelho_do_Liberto'
+  | 'Breve_Tomas'
+  | 'Três_Estelas_de_Sete'
+
+  // Códice V
+  | 'Apócrifo_de_Joao_II'
+  | 'Discurso_de_Baruc'
+  | 'Protênnoia_Triforme_II'
+
+  // Códice VI
+  | 'Epístola_de_Pedro_a_Filipenses'
+  | 'Epístola_dos_Apóstolos'
+  | 'Atos_de_Pedro_e_dos_Apóstolos'
+
+  // Códice VII
+  | 'Evangelho_do_Salvador'
+  | 'Paráfrase_de_Sete'
+
+  // Códice VIII
+  | 'Evangelho_de_Tomas_II'
+  | 'Segunda_Conta_do_Evangelho_de_Tomas';
+
+
+  export type SyriacApocryphalCorpus =
+  | 'Acts_of_Thomas'
+  | 'Doctrine_of_Addai'
+  | 'Acts_of_Mar_Mari'
+  | 'History_of_Simon_Cephas'
+  | 'Exhortation_of_Peter'
+  | 'Travels_of_Peter'
+  | 'Acts_of_Peter_and_the_Twelve_Apostles'
+  | 'Acts_of_Peter'
+  | 'Acts_of_Barnabas'
+  | 'Acts_of_John'
+  | 'Acts_of_John_by_Prochorus'
+  | 'Acts_of_Paul'
+  | 'Acts_of_Paul_and_Thecla'
+  | 'Acts_of_Philip'
+  | 'Acts_of_Pilate'
+  | 'Apocalypse_of_Pseudo_Ezra'
+  | 'Gospel_of_the_Twelve_Apostles'
+  | 'Cave_of_Treasures'
+  | 'Apocalypse_of_John_the_Little'
+  | 'Martyrdom_of_martyred_saints_collections'
+  | 'Lives_of_Saints_and_Martyrs_Collections'
+  | 'Pseudo_Clementine_Romance_Syriac'
+  | 'Acts_and_Miracles_of_Mark'
+  | 'Acts_of_Matthias'
+  | 'Acts_of_Cornelius'
+  | 'Acts_of_Heracleides'
+  | 'Acts_of_Nereus_and_Achilleus'
+  | 'Acts_of_Andrew'
+  | 'Acts_of_Andrew_and_Paul'
+  | 'Acts_of_Stephen'
+  | 'Acts_of_Thaddaeus'
+  | 'Acts_of_Xanthippe_Polyxena_and_Rebecca'
+  | 'Apocryphal_Epistle_to_the_Colossians'
+  | 'Epistle_of_the_Apostles'
+  | 'Epistles_of_Clement'
+  ;
+
+  export type OtherApocryphalCorpus = 
+  | 'Gospel_of_the_Birth_of_Mary'
+  | 'Protevangelion'
+  | 'First_Gospel_of_the_Infancy_of_Jesus_Christ'
+  | 'Thomas_Gospel_of_the_Infancy_of_Jesus_Christ'
+  | 'Epistles_of_Jesus_Christ_and_Abgarus_King_of_Edessa'
+  | 'Gospel_of_Nicodemus_Acts_of_Pontius_Pilate'
+  | 'Apostles_Creed'
+  | 'Epistle_of_Paul_to_the_Laodiceans'
+  | 'Epistles_of_Paul_to_Seneca_and_Seneca_to_Paul'
+  | 'Acts_of_Paul_and_Thecla'
+  | 'First_Epistle_of_Clement_to_the_Corinthians'
+  | 'Second_Epistle_of_Clement_to_the_Corinthians'
+  | 'General_Epistle_of_Barnabas'
+  | 'Epistle_of_Ignatius_to_the_Ephesians'
+  | 'Epistle_of_Ignatius_to_the_Magnesians'
+  | 'Epistle_of_Ignatius_to_the_Trallians'
+  | 'Epistle_of_Ignatius_to_the_Romans'
+  | 'Epistle_of_Ignatius_to_the_Philadelphians'
+  | 'Epistle_of_Ignatius_to_the_Smyrnaeans'
+  | 'Epistle_of_Ignatius_to_Polycarp'
+  | 'Epistle_of_Polycarp_to_the_Philippians'
+  | 'Shepherd_of_Hermas'
+  | 'Second_Book_of_Hermas_Called_His_Commands'
+  | 'Third_Book_of_Hermas_Called_His_Similitudes'
+  | 'Letters_of_Herod_and_Pilate'
+  | 'Lost_Gospel_according_to_Peter';
+
+export type Nostradamus =
+  | 'Oraculos_de_Nostradamus'
+  | 'O_Homem_Que_Viu_Através_Do_Tempo'
+  | 'Nostradamus_Previo_O_Desastre_do_WTC'
+  | 'Almanaque_1555'
+  | 'Almanaque_1556'
+  | 'Almanaque_1557'
+  | 'Almanaque_1558'
+  | 'Almanaque_1559'
+  | 'Almanaque_1560'
+  | 'Almanaque_1561'
+  | 'Almanaque_1562'
+  | 'Almanaque_1563'
+  | 'Almanaque_1564'
+  | 'Almanaque_1565'
+  | 'Almanaque_1566'
+  | 'Almanaque_1567'
+  | 'Centurias_I'
+  | 'Centurias_II'
+  | 'Centurias_III'
+  | 'Centurias_IV'
+  | 'Centurias_V'
+  | 'Centurias_VI'
+  | 'Centurias_VII'
+  | 'Centurias_VIII'
+  | 'Centurias_IX'
+  | 'Centurias_X'
+  | 'Centurias_XI'
+  | 'Centurias_XII';
+
+
+
+export interface Book {
+
+}
+
+export type FinalCodice<BookList extends string> = {
+  [key in BookList]: {}
+}
+
+export interface FinalToraCodice extends FinalCodice<OldTestamentBookList> {
+  
+}
