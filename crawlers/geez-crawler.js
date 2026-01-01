6f205b08f8b7c3003dcc40b8cbb21c109c4b07f0
António Conselheiro <anarcanudos@proton.me>
2026-01-01 09:21:37 -0300

    novo testamento mashafa qeddus

--- crawlers/geez-crawler.js
+++ crawlers/geez-crawler.js
@@ -1,109 +1,136 @@
 const books = [{
   book: 'MAT',
+  finalName: "MAT",
   total: 28
 },
 {
   book: 'MRK',
+  finalName: "MAR",
   total: 16
 },
 {
   book: 'LUK',
+  finalName: "LUC",
   total: 24
 },
 {
   book: 'JHN',
+  finalName: "JOA",
   total: 21
 },
 {
   book: 'ACT',
+  finalName: "ATO",
   total: 28
 },
 {
   book: 'ROM',
+  finalName: "ROM",
   total: 16
 },
 {
   book: '1CO',
+  finalName: "1CO",
   total: 16
 },
 {
   book: '2CO',
+  finalName: "2CO",
   total: 13
 },
 {
   book: 'GAL',
+  finalName: "GAL",
   total: 6
 },
 {
   book: 'EPH',
+  finalName: "EFE",
   total: 6
 },
 {
   book: 'PHP',
+  finalName: "FIL",
   total: 4
 },
 {
   book: 'COL',
+  finalName: "COL",
   total: 4
 },
 {
   book: '1TH',
+  finalName: "1TS",
   total: 5
 },
 {
   book: '2TH',
+  finalName: "2TS",
   total: 3
 },
 {
   book: '1TI',
+  finalName: "1TM",
   total: 6
 },
 {
   book: '2TI',
+  finalName: "2TM",
   total: 4
 },
 {
   book: 'TIT',
+  finalName: "TIT",
   total: 3
 },
 {
   book: 'PHM',
+  finalName: "FLM",
   total: 1
 },
 {
   book: 'HEB',
+  finalName: "HEB",
   total: 13
 },
 {
   book: '1PE',
+  finalName: "1PE",
   total: 5
 },
 {
   book: '2PE',
+  finalName: "2PE",
   total: 3
 },
 {
   book: '1JN',
+  finalName: "1JO",
   total: 5
 },
 {
   book: '2JN',
+  finalName: "2JO",
   total: 1
 },
 {
   book: '3JN',
+  finalName: "3JO",
   total: 1
 },
 {
   book: 'JUD',
+  finalName: "JUD",
   total: 1
 },
 {
   book: 'JAS',
+  finalName: "TIA",
   total: 5
 },
 {
   book: 'REV',
+  finalName: "APO",
   total: 22
 }];
 
@@ -131,6 +158,7 @@ async function fetchSection(book, chapter) {
   }
 }
 
+let verse = null;
 async function loadChapter(book, chapter) {
   return fetchSection(book, chapter).then(text => {
     const parser = new DOMParser();
@@ -143,16 +171,29 @@ async function loadChapter(book, chapter) {
       }
       return span.innerText;
     }).filter(b => !!b.trim()).map(txt => {
-      const [verseNo] = Array.from(txt.match(/^\d+/)),
-        verseText = txt.replace(/^\d+/, '').trim();
-      return {
-        verse: {
-          start: verseNo,
-          end: verseNo,
-          index: index++
-        },
-        text: verseText
+      try {
+        const [verseNo] = Array.from(txt.match(/^\d+/));
+        const verseText = txt
+          .replace(/^\d+/, '')
+          .replace(/(« )/g, '«')
+          .replace(/( »)/g, '»')
+          .trim()
+          .split(' ')
+          .join(' ፡ ');
+
+        verse = {
+          verse: {
+            start: verseNo,
+            end: verseNo,
+            index: index++
+          },
+          text: verseText
+        }
+      } catch (e) {
+        verse.text += `\n${txt.replace(/^\d+/, '').trim()}`;
       }
+
+      return verse;
     }));
   });
 }
@@ -164,34 +205,30 @@ async function loadBook(book, totalChapters) {
     console.log(`📖 Loading ${book} ${chapter}/${totalChapters}`);
     try {
       const verses = await loadChapter(book, chapter);
-      chapters.push({
-        chapter,
-        verses
-      });
+      chapters.push(verses);
 
-      // pequeno delay entre capítulos
-      await sleep(1000);
+      await sleep(10);
     } catch (err) {
       console.error(`❌ Failed loading ${book} ${chapter}`, err);
+      await sleep(3000);
+      chapter--;
     }
   }
 
   return {
-    book,
     chapters
   };
 }
 
 async function crawlAllBooks() {
-  const bible = [];
+  const bible = {};
 
-  for (const { book, total } of books) {
+  for (const { book, finalName, total } of books) {
     console.log(`📚 Starting book ${book}`);
     const bookData = await loadBook(book, total);
-    bible.push(bookData);
+    bible[finalName] = bookData;
 
-    // delay entre livros (educado com o servidor)
-    await sleep(3000);
+    await sleep(10);
   }
 
   return bible;
