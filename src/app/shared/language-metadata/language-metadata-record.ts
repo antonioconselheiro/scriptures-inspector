eb2b3455e5cab30bc92961fd6889579576c7d113
António Conselheiro <anarcanudos@proton.me>
2026-03-23 08:10:50 -0300

    incluindo biblioteca de transliteração

--- src/app/shared/language-metadata/language-metadata-record.ts
+++ src/app/shared/language-metadata/language-metadata-record.ts
@@ -2,7 +2,7 @@ import { transliterate as hebrewTransliterateFn } from "hebrew-transliteration";
 import { Language } from "../../domain/language-model";
 import { LanguageUnionType } from "../../domain/language-union-type";
 import { demassoretifierFn } from "./demassoretifier-fn";
-import { geezTransliterateFn } from "./geez-transliterate-fn";
+import { transliterate } from 'transliteration';
 import { hebrewGematriaFn } from "./hebrew-gematria-fn";
 import { massoretifierFn } from "./massoretifier-fn";
 import { paleoHebrewSpellingFn } from "./paleo-hebrew-spelling-fn";
@@ -13,7 +13,8 @@ export const languageMetadataRecord: {
   'aramaic': {
     name: 'Aramaic',
     label: 'aramaic',
-    direction: 'rtl'
+    direction: 'rtl',
+    transliteration: (aramaic) => transliterate(aramaic)
   },
   'hebrew': {
     name: 'Hebraic',
@@ -41,47 +42,56 @@ export const languageMetadataRecord: {
   'geez': {
     name: 'Ge\'əz',
     label: 'geez',
-    transliteration: (geez) => geezTransliterateFn(geez),
+    transliteration: (geez) => transliterate(geez),
     externalDictionaryLink: 'https://www.geezexperience.com/index.php'
   },
   'syriac': {
     name: 'Syriac',
     label: 'syriac',
+    transliteration: (syriac) => transliterate(syriac),
     direction: 'rtl'
   },
   'arabic': {
     name: 'Arabic',
     label: 'arabic',
+    transliteration: (arabic) => transliterate(arabic),
     direction: 'rtl'
   },
   'latim': {
     name: 'Latim',
-    label: 'latim'
+    label: 'latim',
+    transliteration: (latim) => transliterate(latim)
   },
   'greek': {
     name: 'Greek',
-    label: 'greek'
+    label: 'greek',
+    transliteration: (greek) => transliterate(greek)
   },
   'copta': {
     name: 'Copta',
-    label: 'copta'
+    label: 'copta',
+    transliteration: (copta) => transliterate(copta)
   },
   'sanskrit': {
     name: 'Sanskrit',
-    label: 'sanskrit'
+    label: 'sanskrit',
+    transliteration: (sanskrit) => transliterate(sanskrit)
   },
   'chinese': {
     name: 'Chinese',
-    label: 'chinese'
+    label: 'chinese',
+    transliteration: (chinese) => transliterate(chinese)
   },
   'avestico': {
     name: 'Avestico',
     label: 'avestico',
-    direction: 'rtl'
+    direction: 'rtl',
+    transliteration: (avestico) => transliterate(avestico)
   },
   'pahlavi': {
     name: 'Pahlavi',
     label: 'pahlavi',
-    direction: 'rtl'
+    direction: 'rtl',
+    transliteration: (pahlavi) => transliterate(pahlavi)
   }
 };
